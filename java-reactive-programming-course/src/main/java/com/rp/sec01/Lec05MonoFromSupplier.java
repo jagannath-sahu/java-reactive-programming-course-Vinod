754f01e310de70c3696a66981758c1e4a9b1e0e2
Jagannath Sahu <jagannath8715@gmail.com>
2021-05-26 18:18:25 +0530

    https://www.vinsguru.com/reactor-schedulers-publishon-vs-subscribeon/
    publishOn vs SubscribeOn

--- java-reactive-programming-course/src/main/java/com/rp/sec01/Lec05MonoFromSupplier.java
+++ java-reactive-programming-course/src/main/java/com/rp/sec01/Lec05MonoFromSupplier.java
@@ -2,6 +2,7 @@ package com.rp.sec01;
 
 import com.rp.courseutil.Util;
 import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
 
 import java.util.concurrent.Callable;
 import java.util.function.Supplier;
@@ -25,7 +26,7 @@ public class Lec05MonoFromSupplier {
                         Util.onNext()
                 );
 
-        //here u will get different names
+        //here u will get different names  
         Mono.fromCallable(stringCallable).repeat(4)
                   .subscribe(
                             Util.onNext()
@@ -33,6 +34,16 @@ public class Lec05MonoFromSupplier {
 
         //here always u will get same name
         getName1().repeat(4).subscribe(Util.onNext());
+
+        getName1().repeat(4).subscribeOn(Schedulers.boundedElastic()).subscribe(Util.onNext());
+        try {
+          Thread.sleep(10);
+        } catch (InterruptedException e) {
+          // TODO Auto-generated catch block
+          e.printStackTrace();
+        }
+
+        getName2().repeat(4).subscribe(Util.onNext());
 
     }
 
@@ -44,6 +55,11 @@ public class Lec05MonoFromSupplier {
     private static Mono<String> getName1() {
         System.out.println("Generating name...");
         return Mono.just(Util.faker().name().fullName());
+    }
+
+    private static Mono<String> getName2() {
+      System.out.println("Generating name....");
+      return Mono.just(Util.faker().name().fullName()).subscribeOn(Schedulers.boundedElastic());
     }
 
 }
---
+++ java-reactive-programming-course/src/main/java/com/rp/sec01/PublishOnvsSubscribeOn.java
@@ -0,0 +1,45 @@
+package com.rp.sec01;
+
+import reactor.core.publisher.Flux;
+import reactor.core.scheduler.Schedulers;
+
+public class PublishOnvsSubscribeOn {
+
+  public static void main(String[] args) {
+
+    // https://www.vinsguru.com/reactor-schedulers-publishon-vs-subscribeon/
+
+    Flux<Integer> flux = Flux.range(0, 2)
+        .map(i -> {
+            System.out.println("Mapping one for " + i + " is done by thread " + Thread.currentThread().getName());
+            return i;
+        })
+        .publishOn(Schedulers.boundedElastic())
+        .map(i -> {
+            System.out.println("Mapping two for " + i + " is done by thread " + Thread.currentThread().getName());
+            return i;
+        })
+        .publishOn(Schedulers.parallel())
+        .map(i -> {
+            System.out.println("Mapping three for " + i + " is done by thread " + Thread.currentThread().getName());
+            return i;
+        });
+
+      Runnable r = () -> flux
+          .subscribeOn(Schedulers.single())
+          .subscribe(s -> {
+              System.out.println("Received " + s + " via " + Thread.currentThread().getName());
+          });
+
+      Thread t1 = new Thread(r, "t1");
+      t1.start();
+
+      try {
+        Thread.sleep(100);
+      } catch (InterruptedException e) {
+        // TODO Auto-generated catch block
+        e.printStackTrace();
+      }
+  }
+
+}
