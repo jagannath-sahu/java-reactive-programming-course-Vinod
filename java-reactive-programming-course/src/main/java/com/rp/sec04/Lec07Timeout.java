7339efdfec1549835114faea177796818b2d49d9
Jagannath Sahu <jagannath8715@gmail.com>
2021-06-06 21:58:53 +0530

    timeout real use

--- java-reactive-programming-course/src/main/java/com/rp/sec04/Lec07Timeout.java
+++ java-reactive-programming-course/src/main/java/com/rp/sec04/Lec07Timeout.java
@@ -10,16 +10,20 @@ public class Lec07Timeout {
     public static void main(String[] args) {
 
         getOrderNumbers()
-                .timeout(Duration.ofSeconds(2), fallback())
+                .timeout(Duration.ofSeconds(2), fallback())  // wait for 2 secs
                 .subscribe(Util.subscriber());
 
-      // either can return a fallback value like above or can just log the final end statement
+      // either can return a fallback value like above or can just log the final end statement after error log
 //        getOrderNumbers()
 //                .timeout(Duration.ofSeconds(2))
 //                .doFinally(onFinally -> {
 //                    System.out.println("streaming ended");
 //                }).subscribe(Util.subscriber());
 
+        // can use onErrorResume also in place of doFinally to make complete signal with fallback
+//        getOrderNumbers()
+//                .timeout(Duration.ofSeconds(2))
+//                .onErrorResume((a) -> fallback()).subscribe(Util.subscriber());
 
         Util.sleepSeconds(60);
 
@@ -27,12 +31,12 @@ public class Lec07Timeout {
 
     private static Flux<Integer> getOrderNumbers(){
         return Flux.range(1, 10)
-                    .delayElements(Duration.ofSeconds(5));
+                    .delayElements(Duration.ofSeconds(5)); // releasing in every 5 secs
     }
 
     private static Flux<Integer> fallback(){
         return Flux.range(100, 10)
-                    .delayElements(Duration.ofSeconds(5));
+                    .delayElements(Duration.ofSeconds(5)); // releasing in every 5 secs
     }
 
 }
