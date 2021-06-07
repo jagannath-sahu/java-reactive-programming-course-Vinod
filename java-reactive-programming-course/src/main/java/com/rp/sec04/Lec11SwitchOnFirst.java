42bda4e33a87e14f7f3f339992fa582102619eef
Jagannath Sahu <jagannath8715@gmail.com>
2021-06-07 12:15:08 +0530

    my quiz sourceSubscribedOnce() for testing switchOnFirst method

--- java-reactive-programming-course/src/main/java/com/rp/sec04/Lec11SwitchOnFirst.java
+++ java-reactive-programming-course/src/main/java/com/rp/sec04/Lec11SwitchOnFirst.java
@@ -4,18 +4,23 @@ import com.rp.courseutil.Util;
 import com.rp.sec04.helper.Person;
 import reactor.core.publisher.Flux;
 
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Function;
 
 public class Lec11SwitchOnFirst {
 
     public static void main(String[] args) {
 
+      //signal.get().getAge() will execute only once(remember this) means condition check only for first released element
         getPerson()
                 .switchOnFirst((signal, personFlux) -> {
                     System.out.println("inside switch-on-first");
-                    return signal.isOnNext() && signal.get().getAge() > 10 ? personFlux : applyFilterMap().apply(personFlux);
+                    return signal.isOnNext() && signal.get().getAge() > 20 ? personFlux : applyFilterMap().apply(personFlux);
                 })
+                // .filter(e -> false)
                 .subscribe(Util.subscriber());
+
+        //sourceSubscribedOnce();
     }
 
     public static Flux<Person> getPerson(){
@@ -25,12 +30,22 @@ public class Lec11SwitchOnFirst {
 
     public static Function<Flux<Person>, Flux<Person>> applyFilterMap(){
         return flux -> flux
-                .filter(p -> p.getAge() > 10)
+                .filter(p -> p.getAge() > 20)
                 .doOnNext(p -> p.setName(p.getName().toUpperCase()))
                 .doOnDiscard(Person.class, p -> System.out.println("Not allowing : " + p));
     }
 
+    public static void sourceSubscribedOnce() {
+      AtomicInteger subCount = new AtomicInteger();
+      Flux<Integer> source = Flux.range(1, 10)
+                                 .hide()
+                                 .doOnSubscribe(subscription -> subCount.incrementAndGet());
 
+      // s.get() will execute once only so will get value in this case 1(important remember this)
+      source.switchOnFirst((s, f) -> f.filter(v -> v % 2 == s.get())).subscribe(Util.subscriber());
 
+      //subCount should be 1
+      System.out.println(subCount);
+    }
 
 }
