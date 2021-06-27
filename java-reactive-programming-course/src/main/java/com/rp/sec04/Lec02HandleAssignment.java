1739ebee7d8af74c5a8ce16fa87ea18c9e8eb5cd
Jagannath Sahu <jagannath8715@gmail.com>
2021-06-27 19:17:25 +0530

    commenting Object::toString and adding typecasting
    public static <T> Flux<T> generate(Consumer<SynchronousSink<T>>
    generator)
    {
    	Objects.requireNonNull(generator, "generator");
    	return onAssembly(new FluxGenerate<>(generator));
    }
    
    generate is a generic method.
    
    Example is below:
    
    import java.util.*;
    public class HelloWorld {
    
        public static class Util {
            public static <T> T getValue(Object obj, Class<T> clazz) {
                return (T) obj;
            }
            public static <T> T getValue(Object obj) {
                return (T) obj;
            }
        }
    
        public static void main(String []args) {
    		List list = Arrays.asList("Author", "Book");
    		for (Object element : list) {
    		    String data = Util.getValue(element, String.class);
    		    System.out.println(data);
    		    System.out.println(Util.<String>getValue(element));
    		}
        }
    }
    
    https://codegym.cc/groups/posts/generics-in-java

--- java-reactive-programming-course/src/main/java/com/rp/sec04/Lec02HandleAssignment.java
+++ java-reactive-programming-course/src/main/java/com/rp/sec04/Lec02HandleAssignment.java
@@ -7,8 +7,8 @@ public class Lec02HandleAssignment {
 
     public static void main(String[] args) {
 
-        Flux.generate(synchronousSink -> synchronousSink.next(Util.faker().country().name()))
-                .map(Object::toString)
+        Flux.<String>generate(synchronousSink -> synchronousSink.next(Util.faker().country().name()))
+                //.map(Object::toString)
                 .handle((s, synchronousSink) -> {
                     synchronousSink.next(s);
                     if(s.toLowerCase().equals("canada"))
